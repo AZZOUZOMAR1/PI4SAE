54e181555b354a26c2d6c38467ea67db3ecb0120
omar azzouz <azzouzo317@gmail.com>
2026-02-02 12:18:19 +0100

    Premier commit

---
+++ Back_End/micro_services/Condidat/src/main/java/org/example/condidat/CondidatApplication.java
@@ -0,0 +1,13 @@
+package org.example.condidat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondidatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CondidatApplication.class, args);
+    }
+
+}
